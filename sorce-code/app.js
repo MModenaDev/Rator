5dd3480f4c5cf40ad837e171556accd3699dab3e
Anedom <marco.modena@tutanota.com>
2019-10-05 13:59:43 -0300

    nodemon, cookies n session

--- sorce-code/app.js
+++ sorce-code/app.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 
+const bodyParser   = require("body-parser");
+const session      = require("express-session");
+const MongoStore   = require("connect-mongo")(session);
+const cookieParser = require('cookie-parser');
 const express      = require('express');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
@@ -20,6 +24,18 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 const app = express();
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
