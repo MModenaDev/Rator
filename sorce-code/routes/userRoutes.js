6e0ed911cf8ce9ba916f3939ca8286e3840c4ae3
Anedom <marco.modena@tutanota.com>
2019-10-05 11:36:36 -0300

    routes minimal gets

--- sorce-code/routes/userRoutes.js
+++ sorce-code/routes/userRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router  = express.Router();
 
+router.get('/', (req, res, next) => {
+    res.render('users/profile');
+});
 
+router.get('/edit', (req, res, next) => {
+    res.render('users/edit');
+});
 
 module.exports = router;
